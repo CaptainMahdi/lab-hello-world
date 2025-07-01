8d3340a4150255a3ccc4341e921d6d188b6bb62f
Mahdi <mahdi.hteit@gmail.com>
2025-07-01 14:05:18 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+print("Hello from Mahdi!")
+print("---------------------")
+
+name = input("Enter your name: ")
+
+if name == "mahdi":
+    print("Hello Boss!")
+else:
+    print("Hello " + name + "!")
+    
+age = int(input("How old are you? "))
+if 100 >age >= 18:
+    print("You are an adult.")
+elif age > 100:
+    print("!!DAMN " + name.upper() + " UR OLD!!")
+elif age < 18:
+    print("You are a minor.")
+    
+print("---------------------")
+
+if name == "mahdi" and age == 14:
+    print("Identity confirmed. Welcome back Mahdi!")
+else:
+    print("You are all signed up!")
